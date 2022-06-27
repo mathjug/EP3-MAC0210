dbdd35357b342554ce6c7b8dad3f009eeaa5e75e
mathjug <mjurgensen@usp.br>
2022-06-27 17:51:18 -0300

    implementa o método de monte carlo unidimensional.

--- parte2.c
+++ parte2.c
@@ -1,13 +1,46 @@
 /*
     EP3 MAC0210 - PARTE 2
     ALUNOS: Matheus Sanches Jurgensen (12542199) e André Nogueira Ribeiro (12542230)
-    COMO COMPILAR: gcc -o parte2 parte2.c
+    COMO COMPILAR: gcc -o parte2 parte2.c -lm
     COMO EXECUTAR: ./parte2
 */
 
 #include <stdio.h>
 #include <stdlib.h> // rand() e srand()
 #include <time.h> // time()
+#include <math.h>
+
+double uniMonteCarlo(int n, double (*f)(double)) {
+    /*
+    Implementa o Método de Monte Carlo para o caso univariado. Nesse sentido, são 2 os seus parâmetros:
+
+    n = quantidade de número aleatórios a serem gerados
+    f = função cuja integral deseja-se aproximar, a qual recebe um double como seu argumento e retorna
+        outro double
+    
+    Retorna o valor da integral aproximado pelo método.
+    */
+    double valor_final = 0;
+    srand(time(NULL));
+    for (int i = 0; i < n; i++) {
+        double x = ((double)rand() / (double)RAND_MAX);
+        valor_final += f(x);
+    }
+    valor_final = (valor_final / n);
+    return valor_final;
+}
+
+double seno(double x) {
+    return sin(x);
+}
+
+double cubo(double x) {
+    return 4 * pow(4*x + 3, 3);
+}
+
+double exponencial(double x) {
+    return 1;
+}
 
 double multiMonteCarlo(int n, int m, double (*f)(double*)) {
     /*
@@ -34,7 +67,7 @@ double multiMonteCarlo(int n, int m, double (*f)(double*)) {
 
 double auxCalcularPi(double* variaveis) {
     /*
-    Método auxiliar para a aproximação do valor de pi. Recebe um vetor do tipo [x,y]e verifica se o ponto
+    Método auxiliar para a aproximação do valor de pi. Recebe um vetor do tipo [x,y] e verifica se o ponto
     (x,y) está contido no círculo centrado na origem e de raio unitário.
     */
     double x = variaveis[0];
@@ -52,4 +85,6 @@ int main() {
     scanf("%d", &n);
     double pi = multiMonteCarlo(n, 2, auxCalcularPi);
     printf("O valor de pi é: %f\n", pi);
+    double cube = uniMonteCarlo(n, cubo);
+    printf("O valor da integral do cubo é: %f\n", cube);
 }
